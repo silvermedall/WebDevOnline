e6d68ddd40a1d9a123c58e8f7f25f38e33ff3c67
silvermedall <toxicsilver9@gmail.com>
2025-04-16 23:12:17 +0100

    test build

--- Infill Shield/frontend/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <div className="header">
-      <div className="header-wall-left" />
-      <img src="./src/assets/logo.svg" className="logo" />
-      <div className="header-wall-right" />
-    </div>
-  );
-}
